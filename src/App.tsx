8d91800d5581b39354ef364cd561cade12af09c5
ejcondorf88 <eliancondor2523658@gmail.com>
2026-03-31 19:58:56 -0500

    feat:add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './router/AppRoutes'
 
 const App: FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">React App</h1>
-        <p className="text-gray-600">Proyecto configurado y listo para desarrollar</p>
-      </div>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ src/router/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from '@/pages/HomePage'
+import { CreditCardsPage } from '@/pages/CreditCardsPage'
+import { NotFoundPage } from '@/pages/NotFoundPage'
+
+export const AppRoutes: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/credit-cards" element={<CreditCardsPage />} />
+      <Route path="/credit-cards/:id" element={<CreditCardsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
